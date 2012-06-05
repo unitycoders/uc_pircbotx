992b01e8b074b31a637b6136eaa774549759e953
Bruce Cowan <bruce@bcowan.me.uk>
2012-06-05 21:30:25 +0100

    Add copyright headers
    Instantly doubling the lines of code...

--- src/uk/co/unitycoders/pircbotx/Bot.java
+++ src/uk/co/unitycoders/pircbotx/Bot.java
@@ -1,4 +1,21 @@
-//TODO GPLv3
+/**
+ * Copyright © 2012 Bruce Cowan <bruce@bcowan.me.uk>
+ *
+ * This file is part of uc_PircBotX.
+ *
+ * uc_PircBotX is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * uc_PircBotX is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with uc_PircBotX.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package uk.co.unitycoders.pircbotx;
 
 import org.pircbotx.PircBotX;
