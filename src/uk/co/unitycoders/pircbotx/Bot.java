6d73aaa6706767faa1a44fa757d5cbe8b05fbdf8
Bruce Cowan <bruce@bcowan.me.uk>
2012-06-06 11:09:42 +0100

    Actually add LinesListener to the ListenerManager

--- src/uk/co/unitycoders/pircbotx/Bot.java
+++ src/uk/co/unitycoders/pircbotx/Bot.java
@@ -26,6 +26,7 @@ import uk.co.unitycoders.pircbotx.commands.KillerTroutCommand;
 import uk.co.unitycoders.pircbotx.commands.LartCommand;
 import uk.co.unitycoders.pircbotx.commands.RandCommand;
 import uk.co.unitycoders.pircbotx.commands.DateTimeCommand;
+import uk.co.unitycoders.pircbotx.listeners.LinesListener;
 
 public class Bot
 {
@@ -38,6 +39,7 @@ public class Bot
 		manager.addListener(new JoinsCommand());
 		manager.addListener(new KillerTroutCommand());
 		manager.addListener(new LartCommand());
+		manager.addListener(new LinesListener());
 		manager.addListener(new RandCommand());
 
 		try
