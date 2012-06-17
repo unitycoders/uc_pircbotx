f3868d12d697a14c3aa79e40f0e31c5338ec81c1
Bruce Cowan <bruce@bcowan.me.uk>
2012-06-17 18:48:57 +0100

    [JoinsListener] Add copyright header

--- src/main/java/uk/co/unitycoders/pircbotx/listeners/JoinsListener.java
+++ src/main/java/uk/co/unitycoders/pircbotx/listeners/JoinsListener.java
@@ -1,3 +1,21 @@
+/**
+ * Copyright © 2012 Bruce Cowan <bruce@bcowan.me.uk>
+ *
+ * This file is part of uc_PircBotX.
+ *
+ * uc_PircBotX is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * uc_PircBotX is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with uc_PircBotX.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package uk.co.unitycoders.pircbotx.listeners;
 
 import java.util.Map;
@@ -18,6 +36,11 @@ public class JoinsListener extends ListenerAdapter<PircBotX>
 		this.joins = new TreeMap<User, Integer>();
 	}
 
+	/**
+	 * Gets the {@link JoinsListener} singleton.
+	 *
+	 * @return the JoinsListener singleton
+	 */
 	public static JoinsListener getInstance()
 	{
 		if (instance == null)
