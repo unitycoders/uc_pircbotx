00f7bcf77c33af9e41cf4d94ecf54818e1302287
Bruce Cowan <bruce@bcowan.me.uk>
2015-01-16 18:55:15 +0000

    Unify copyright headers

--- src/main/java/uk/co/unitycoders/pircbotx/commandprocessor/ChannelMessage.java
+++ src/main/java/uk/co/unitycoders/pircbotx/commandprocessor/ChannelMessage.java
@@ -1,9 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Copyright © 2013-2015 Unity Coders
+ *
+ * This file is part of uc_pircbotx.
+ *
+ * uc_pircbotx is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * uc_pircbotx is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * uc_pircbotx. If not, see <http://www.gnu.org/licenses/>.
  */
-
 package uk.co.unitycoders.pircbotx.commandprocessor;
 
 import org.pircbotx.PircBotX;
--- src/test/java/uk/co/unitycoders/pircbotx/commandprocessor/CommandProcessorTest.java
+++ src/test/java/uk/co/unitycoders/pircbotx/commandprocessor/CommandProcessorTest.java
@@ -1,20 +1,20 @@
 /**
- * Copyright © 2013 Joseph Walton-Rivers <webpigeon@unitycoders.co.uk>
+ * Copyright © 2013-2015 Unity Coders
  *
- * This file is part of uc_PircBotX.
+ * This file is part of uc_pircbotx.
  *
- * uc_PircBotX is free software: you can redistribute it and/or modify it under
+ * uc_pircbotx is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
  *
- * uc_PircBotX is distributed in the hope that it will be useful, but WITHOUT
+ * uc_pircbotx is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
  *
  * You should have received a copy of the GNU General Public License along with
- * uc_PircBotX. If not, see <http://www.gnu.org/licenses/>.
+ * uc_pircbotx. If not, see <http://www.gnu.org/licenses/>.
  */
 package uk.co.unitycoders.pircbotx.commandprocessor;
 
