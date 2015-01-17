9ac23eba3ef28b145bf7cbcc0134278d0a2e1050
Bruce Cowan <bruce@bcowan.me.uk>
2015-01-17 11:35:53 +0000

    Tidy karma
    Use only spaces from now on, no tabs

--- src/main/java/uk/co/unitycoders/pircbotx/commands/KarmaCommand.java
+++ src/main/java/uk/co/unitycoders/pircbotx/commands/KarmaCommand.java
@@ -21,6 +21,9 @@ package uk.co.unitycoders.pircbotx.commands;
 import java.sql.SQLException;
 import java.util.Collection;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import uk.co.unitycoders.pircbotx.commandprocessor.Command;
 import uk.co.unitycoders.pircbotx.commandprocessor.Message;
 import uk.co.unitycoders.pircbotx.data.db.DBConnection;
@@ -29,60 +32,59 @@ import uk.co.unitycoders.pircbotx.types.Karma;
 
 public class KarmaCommand {
 
+    private final Logger logger = LoggerFactory.getLogger(KarmaCommand.class);
     private KarmaModel model;
 
     public KarmaCommand() {
         try {
             this.model = DBConnection.getKarmaModel();
         } catch (Exception ex) {
-            ex.printStackTrace();
+            logger.error("Database error", ex);
         }
     }
 
     @Command("default")
-    public void onKarma(Message event) throws Exception {
+    public void onKarma(Message event) {
         String target = event.getArgument(2, null);
 
         int karma = this.model.getKarma(target);
         event.respond("Karma for " + target + " = " + karma);
     }
-    
+
     @Command("top")
-    public void topKarma(Message event) throws Exception {
-    	Collection<Karma> karmaList = model.getTopKarma(5);
-    	
-    	StringBuffer buffer = new StringBuffer();
-    	for (Karma karma : karmaList) {
-    		buffer.append(karma.toString());
-    		buffer.append(" ");
-    	}
-    	
-    	event.respond(buffer.toString());
+    public void topKarma(Message event) {
+        Collection<Karma> karmaList = model.getTopKarma(5);
+
+        StringBuilder builder = new StringBuilder();
+        for (Karma karma : karmaList) {
+            builder.append(karma.toString());
+            builder.append(" ");
+        }
+
+        event.respond(builder.toString());
     }
 
-    @Command({"increment", "add"})
-    public void incrementKarma(Message event) throws Exception {
+    @Command({ "increment", "add" })
+    public void incrementKarma(Message event) {
         String target = event.getArgument(2, null);
 
         try {
             int karma = this.model.incrementKarma(target);
             event.respond("Karma for " + target + " is now " + karma);
         } catch (SQLException ex) {
-            event.respond("Database error: " + ex.getMessage());
-            ex.printStackTrace();
+            logger.error("Database error", ex);
         }
     }
 
-    @Command({"decrement", "remove"})
-    public void decrementKarma(Message event) throws Exception {
+    @Command({ "decrement", "remove" })
+    public void decrementKarma(Message event) {
         String target = event.getArgument(2, null);
 
         try {
             int karma = this.model.decrementKarma(target);
             event.respond("Karma for " + target + " is now " + karma);
         } catch (SQLException ex) {
-            event.respond("Database error: " + ex.getMessage());
-            ex.printStackTrace();
+            logger.error("Database error", ex);
         }
     }
 }
