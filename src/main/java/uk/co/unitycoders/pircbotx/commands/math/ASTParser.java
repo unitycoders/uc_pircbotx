cb7dd18ab70848e319af35556fc2ab53322d4884
leonchen83 <chen.bao.yi@qq.com>
2016-01-02 01:32:15 +0800

    ast tree

--- src/main/java/uk/co/unitycoders/pircbotx/commands/math/ASTParser.java
+++ src/main/java/uk/co/unitycoders/pircbotx/commands/math/ASTParser.java
@@ -1,118 +1,209 @@
 package uk.co.unitycoders.pircbotx.commands.math;
 
-import java.util.Map;
-import java.util.Queue;
-import java.util.Stack;
+import uk.co.unitycoders.pircbotx.commands.math.ast.*;
+
+import java.util.*;
+
+import static uk.co.unitycoders.pircbotx.commands.math.TokenType.*;
 
 /**
  * see https://www.engr.mun.ca/~theo/Misc/exp_parsing.htm (Thanks leonchen83)
- * 
- * @author webpigeon
  *
+ * @author webpigeon
  */
 public class ASTParser {
-	private static final TokenType V = TokenType.NUMBER; //TODO find out what v corrisponds to, assumption value/number
-	private static final Token SENTINAL = null;
-	
-	private Map<String, Function> functions;
-	private Stack<Token> operators;
-	private Stack<TreeNode> operands;
-	
-	//TODO rename functions to be easier to follow
-	
-	public ASTParser(Map<String,Function> functions) {
-		this.functions = functions;
-	}
-	
-	public TreeNode process(Queue<Token> input) {
-		operators = new Stack<>();
-		operands = new Stack<>();
-		
-		operators.push(SENTINAL);
-		e(input);
-		
-		if (input.isEmpty()) {
-			throw new RuntimeException("Expected end of tokens but wasn't");
-		}
-		
-		return operands.pop(); //stop the compiler complaining.
-	}
-	
-	public void e(Queue<Token> input) {
-		p(input);
-		
-		int arity = -1;
-		do {
-			
-			Token next = input.poll();
-			if (next.isType(TokenType.OPERATOR)) {
-				Function nextFunction = functions.get(next.getValue());
-				arity = nextFunction.getArity();
-				
-				pushOperator(next);
-				input.poll();
-				p(input);	
-			}
-			
-		} while (arity == 2);
-		
-		Token next = operators.peek();
-		while (next != SENTINAL) {
-			popOperator();
-		}
-		
-	}
-	
-	public void p(Queue<Token> input) {
-		Token next = input.peek();
-		
-		if (next.isType(V)) {
-			operands.push(new TreeNode(next));
-			input.poll();
-		} else if (next.isType(TokenType.L_PARA)) {
-			operators.push(SENTINAL);
-			e(input);
-			
-			next = input.poll();
-			if (next.isType(TokenType.R_PARA)) {
-				throw new RuntimeException("Expected closing bracket");
-			}
-		} else if (next.isType(TokenType.OPERATOR)) {
-			Function nextFunction = functions.get(next.getValue());
-			if (nextFunction.getArity() == 1) {
-				pushOperator(next);
-				input.poll();
-				p(input);
-			}
-		} else {
-			System.out.println("didn't know what to do with "+next.getType());
-			throw new RuntimeException("nope.");
-		}
-	}
-	
-	public void popOperator() {
-		Token top = operators.peek();
-		Function topFunction = functions.get(top);
-		
-		if (topFunction.getArity() == 2) { 
-			TreeNode t1 = operands.pop();
-			TreeNode t0 = operands.pop();
-			operands.push(new TreeNode(operators.pop(), t0, t1));
-		} else {
-			operands.push(new TreeNode(operators.pop(), operands.pop()));
-		}
-	}
-	
-	public void pushOperator(Token op) {
-		Token top = operators.peek();
-		
-		Function topFunction = functions.get(top.getValue());
-		Function opFunction = functions.get(op.getValue());
-		
-		while (topFunction.getPrecidence() > opFunction.getPrecidence()) {
-			popOperator();
-		}
-		operators.push(op);
-	}
-	
+    private static final TokenType V = TokenType.NUMBER; //TODO find out what v corrisponds to, assumption value/number
+    private static final Token SENTINAL = null;
+
+    private Map<String, Function> functions;
+    private Stack<Token> operators;
+    private Stack<TreeNode> operands;
+
+    private Queue<Token> input;
+
+    private Token curr;
+
+    public ASTParser(Queue<Token> input, Map<String, Function> functions) {
+        this.input = input;
+        this.functions = functions;
+        next();
+    }
+
+    private final List<List<Object>> suffixExprListSupply = new ArrayList<>();
+
+    private final List<Stack<Function>> opStackSupply = new ArrayList<>();
+
+    private List<Object> newSuffixExprList() {
+        suffixExprListSupply.add(new ArrayList<>());
+        return suffixExprListSupply.get(suffixExprListSupply.size() - 1);
+    }
+
+    private Stack<Function> newOpStack() {
+        opStackSupply.add(new Stack<Function>());
+        return opStackSupply.get(opStackSupply.size() - 1);
+    }
+
+    /**
+     * expr = expr + expr
+     * expr = expr - expr
+     * expr = expr * expr
+     * expr = expr / expr
+     * expr = expr ^ expr
+     * expr = expr % expr
+     * expr = (expr)
+     * expr = num
+     * expr = ^expr
+     * expr = func({expr[,expr]...}) //only 1 argument?
+     *
+     */
+
+    /**
+     * remove left recursion
+     *
+     * @see [https://en.wikipedia.org/wiki/Left_recursion]
+     * <p/>
+     * expr = (expr) expr'
+     * | func({expr[,expr]...}) expr'
+     * | num expr'
+     * <p/>
+     * <p/>
+     * <p/>
+     * expr' =  + expr expr'
+     * | - expr expr'
+     * | * expr expr'
+     * | / expr expr'
+     * | ^ expr expr'
+     * | % expr expr'
+     * | ε
+     */
+
+    public Expr expr() {
+        newSuffixExprList();
+        newOpStack();
+        Expr expr = expr0();
+        suffixExprListSupply.remove(suffixExprListSupply.size() - 1);
+        opStackSupply.remove(opStackSupply.size() - 1);
+        return expr;
+    }
+
+    public Expr expr0() {
+        if (curr.isType(L_PARA)) {
+            accept(L_PARA);
+            add(expr());
+            accept(R_PARA);
+            return exprRest();
+        } else if (curr.isType(FUNCTION)) {
+            String funcName = curr.getValue();
+            accept(FUNCTION);
+            accept(L_PARA);
+            Expr expr = expr();
+            accept(R_PARA);
+            FuncExpr funcExpr = new FuncExpr(funcName, expr);
+            add(funcExpr);
+            return exprRest();
+        } else if (curr.isType(NUMBER)) {
+            String num = curr.getValue();
+            add(new NumExpr(num));
+            accept(NUMBER);
+            return exprRest();
+        } else {
+            throw new RuntimeException("Expected ['(','func','num'] but actual " + curr.getType());
+        }
+    }
+
+    public Expr exprRest() {
+        if (curr == null) {
+            pushOp(new OperatorFunction("$", true, 0));
+            List<Object> suffixExpr = suffixExprListSupply.get(opStackSupply.size() - 1);
+            return ast(suffixExpr);
+        } else if (curr.isType(OPERATOR)) {
+            String operator = curr.getValue();
+            OperatorFunction function = (OperatorFunction) functions.get(operator);
+            pushOp(function);
+            accept(OPERATOR);
+            expr0();
+            return exprRest();
+        } else {
+            pushOp(new OperatorFunction("$", true, 0));
+            List<Object> suffixExpr = suffixExprListSupply.get(opStackSupply.size() - 1);
+            return ast(suffixExpr);
+        }
+    }
+
+    private Expr ast(List<Object> suffixExpr) {
+        Stack<Object> stack = new Stack<>();
+        for (int i = 0; i < suffixExpr.size(); i++) {
+            if (suffixExpr.get(i) instanceof OperatorFunction) {
+                OperatorFunction op = (OperatorFunction) suffixExpr.get(i);
+                if (op.getName().equals("+")) {
+                    Expr right = (Expr) stack.pop();
+                    Expr left = (Expr) stack.pop();
+                    stack.push(new AddExpr(left, right));
+                } else if (op.getName().equals("-")) {
+                    Expr right = (Expr) stack.pop();
+                    Expr left = (Expr) stack.pop();
+                    stack.push(new SubExpr(left, right));
+                } else if (op.getName().equals("*")) {
+                    Expr right = (Expr) stack.pop();
+                    Expr left = (Expr) stack.pop();
+                    stack.push(new MultiExpr(left, right));
+                } else if (op.getName().equals("/")) {
+                    Expr right = (Expr) stack.pop();
+                    Expr left = (Expr) stack.pop();
+                    stack.push(new DivideExpr(left, right));
+                } else if (op.getName().equals("^")) {
+                    Expr right = (Expr) stack.pop();
+                    Expr left = (Expr) stack.pop();
+                    stack.push(new PowExpr(left, right));
+                } else if (op.getName().equals("%")) {
+                    Expr right = (Expr) stack.pop();
+                    Expr left = (Expr) stack.pop();
+                    stack.push(new ModExpr(left, right));
+                }
+            } else {
+                stack.push(suffixExpr.get(i));
+            }
+        }
+        assert stack.size() == 1;
+        return (Expr) stack.pop();
+    }
+
+    private void add(Expr expr) {
+        suffixExprListSupply.get(suffixExprListSupply.size() - 1).add(expr);
+    }
+
+    private void add(Function function) {
+        suffixExprListSupply.get(suffixExprListSupply.size() - 1).add(function);
+    }
+
+    private void pushOp(OperatorFunction function) {
+        Stack<Function> opStack = opStackSupply.get(opStackSupply.size() - 1);
+        String o1 = function.getName();
+        if (functions.get(o1) != null) {
+            if (opStack.isEmpty()) {
+                opStack.push(function);
+            } else {
+                while (!opStack.isEmpty() && ((function.isLeftAssoc() && function.getPrecidence() <= opStack.peek().getPrecidence()) || (!function.isLeftAssoc() && function.getPrecidence() < opStack.peek().getPrecidence()))) {
+                    add(opStack.pop());
+                }
+                opStack.push(function);
+            }
+        } else {
+            while (!opStack.isEmpty()) {
+                add(opStack.pop());
+            }
+        }
+    }
+
+    public void accept(TokenType type) {
+        if (curr.isType(type)) {
+            next();
+        } else {
+            throw new RuntimeException("Expected " + type + " but actual " + curr);
+        }
+    }
+
+    public void next() {
+        curr = input.poll();
+    }
 }
--- src/test/java/uk/co/unitycoders/pircbotx/commands/math/TreeParserTest.java
+++ src/test/java/uk/co/unitycoders/pircbotx/commands/math/TreeParserTest.java
@@ -8,16 +8,29 @@ import java.util.TreeMap;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import uk.co.unitycoders.pircbotx.commands.math.ast.Expr;
 
 public class TreeParserTest {
 	private static final Double ERROR_MARGIN = 0.00000000001; //something really small
 	private ASTParser parser;
-	
+
+    Map<String, Function> functions;
+
 	@Before
 	public void setup() {
-		Map<String, Function> functions = new TreeMap<String, Function>();
+		functions = new TreeMap<String, Function>();
+        functions.put("+", new OperatorFunction("+", true, 2));
+        functions.put("-", new OperatorFunction("-", true, 2));
+        functions.put("/", new OperatorFunction("/", true, 3));
+        functions.put("*", new OperatorFunction("*", true, 3));
+        functions.put("%", new OperatorFunction("%", true, 3));
+        functions.put("^", new OperatorFunction("^", false, 4));
+        functions.put("SIN", new MathFunction("SIN", true, 5));
+        functions.put("COS", new MathFunction("COS", true, 5));
+        functions.put("TAN", new MathFunction("TAN", true, 5));
+        functions.put("DEG", new MathFunction("DEG", true, 5));
+        functions.put("RAD", new MathFunction("RAD", true, 5));
 
-		this.parser = new ASTParser(functions);
 	}
 	
 	@Test
@@ -25,11 +38,11 @@ public class TreeParserTest {
 		//build infix queue
 		Queue<Token> tokens = new LinkedList<>();
 		tokens.add(new Token(TokenType.NUMBER, "5"));
-		
+        this.parser = new ASTParser(tokens,functions);
 		//convert to postfix
-		TreeNode reordered = parser.process(tokens); //??
-		System.out.println(reordered);
-		
+		Expr expr = parser.expr(); //??
+        System.out.println(expr);
+
 		//check they match
 		/*Token first = reordered.poll();
 		Assert.assertEquals(first, new Token(TokenType.NUMBER, "5"));
@@ -42,25 +55,26 @@ public class TreeParserTest {
 		Queue<Token> tokens = new LinkedList<>();
 		tokens.add(new Token(TokenType.NUMBER, "5"));
 		tokens.add(new Token(TokenType.OPERATOR, "+"));
-		tokens.add(new Token(TokenType.NUMBER, "5"));
-		
+		tokens.add(new Token(TokenType.NUMBER, "6"));
+
+        this.parser = new ASTParser(tokens,functions);
 		//convert to postfix
-		TreeNode reordered = parser.process(tokens); //??
+		Expr reordered = parser.expr(); //??
 		System.out.println(reordered);
-		
+
 		//check they match
 		/*Token first = reordered.poll();
 		Assert.assertEquals(first, new Token(TokenType.NUMBER, "5"));
-		
+
 		Token second = reordered.poll();
 		Assert.assertEquals(second, new Token(TokenType.NUMBER, "5"));
-		
+
 		Token last = reordered.poll();
 		Assert.assertEquals(last, new Token(TokenType.OPERATOR, "+"));
-		
+
 		Assert.assertEquals(true, reordered.isEmpty());*/
 	}
-	
+
 	@Test
 	public void testParseFunction() {
 		//build infix queue
@@ -69,24 +83,26 @@ public class TreeParserTest {
 		tokens.add(new Token(TokenType.L_PARA));
 		tokens.add(new Token(TokenType.NUMBER, "5"));
 		tokens.add(new Token(TokenType.R_PARA));
-		
+
+        this.parser = new ASTParser(tokens,functions);
 		//convert to postfix
-		TreeNode reordered = parser.process(tokens); //??
+		Expr reordered = parser.expr(); //??
 		System.out.println(reordered);
-		
+
 		/*System.out.println("REORDERED "+reordered); //lies.
-		
+
 		//check they match
 		Token first = reordered.poll();
 		Assert.assertEquals(first, new Token(TokenType.NUMBER, "5"));
-		
+
 		Token second = reordered.poll();
 		Assert.assertEquals(second, new Token(TokenType.FUNCTION, "COS"));
-		
+
 		Assert.assertEquals(true, reordered.isEmpty());*/
 	}
-	
-	
+
+
+
 	@Test
 	public void testParse2Functions() {
 		//build infix queue
@@ -100,11 +116,12 @@ public class TreeParserTest {
 		tokens.add(new Token(TokenType.L_PARA));
 		tokens.add(new Token(TokenType.NUMBER, "5"));
 		tokens.add(new Token(TokenType.R_PARA));
-		
+
+        this.parser = new ASTParser(tokens,functions);
 		//convert to postfix
-		TreeNode reordered = parser.process(tokens); //??
+		Expr reordered = parser.expr(); //??
 		System.out.println(reordered);
-		
+
 		//So we expect elements in postfix because we pop elements off the stack
 		//then do what they mean and pop the result back on
 		//we need the elements to therefore be in arguments then function order
@@ -115,13 +132,13 @@ public class TreeParserTest {
 				new Token(TokenType.FUNCTION, "COS"),
 				new Token(TokenType.OPERATOR, "+")
 		};
-		
+
 		//check they match
 		for (Token expToken : expTokens) {
 			Token next = reordered.poll();
 			Assert.assertEquals(expToken, next);
 		}
-		
+
 		//check there are no more
 		Assert.assertEquals(true, reordered.isEmpty());*/
 	}
