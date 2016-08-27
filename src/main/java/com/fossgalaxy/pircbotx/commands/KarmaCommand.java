128c19ba84b30ed719632b3cb3bb4cef702d8fbd
Joseph Walton-Rivers <joseph@webpigeon.me.uk>
2016-08-27 14:19:39 +0100

    move joins command to own package

--- src/main/java/com/fossgalaxy/pircbotx/commands/KarmaCommand.java
+++ src/main/java/com/fossgalaxy/pircbotx/commands/KarmaCommand.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License along with
  * uc_pircbotx. If not, see <http://www.gnu.org/licenses/>.
  */
-package com.fossgalaxy.pircbotx.commands;
+package com.fossgalaxy.pircbotx.commands.karma;
 
 import com.fossgalaxy.pircbotx.commandprocessor.Command;
 import com.fossgalaxy.pircbotx.commandprocessor.Message;
